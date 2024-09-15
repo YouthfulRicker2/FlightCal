e91acc2c5bd345c5d3027a83f96e74e017fa2eb7
Antariksh Anand <antarikshanand@gmail.com>
2024-09-16 10:40:36 +1200

    PROGRAM START

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Hello World")
+info = {
+    "name" : "",
+    "class" : ""
+}
+
+cost_info = {
+    "init_cost" : "",
+    "total_seats" : "",
+    "remaining_seats" : "",
+}
+
+class_list = ["economy", "prem_economy", "business", "first"]
+
+def cost_creator(cost_info);
+
+print("Welcome to FlightCal!\nPlease state the name of your client.")
