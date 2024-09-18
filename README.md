47eac11cbc94df4e8e3e01394ca054c46b86701e
Antariksh Anand <antarikshanand@gmail.com>
2024-09-18 22:04:11 +1200

    Skeleton Creation
    I have started creating what is the backbone of the program, with the basic dictionaries and questions. More to come.

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 ## Overview
 
-This is a project that I, Antariksh, am doing as a part of my DIGITEC2 class at Wainuiomata High School.
+This is some work for my 2024 NCEA Level 2 Digitech Class at [Wainuiomata High School](https://wainuiomatahigh.school.nz/).
+
+## Main Project Directory
+
+This is an app that will determine your theoretical discounted flight price using factors such as flight class, empty seat quantity, and type of discount.
+It will be posed and framed as a inward-facing application for clerks and other consumer-facing workers. 
+This will be an internal application and as such, will be designed and have prompts phrased accordingly.
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 info = {
     "name" : "",
+    "age" : "",
     "class" : ""
 }
 
@@ -9,8 +10,10 @@ cost_info = {
     "remaining_seats" : "",
 }
 
-class_list = ["economy", "prem_economy", "business", "first"]
+class_list = ["Economy", "Premium_Economy", "Business", "First"]
 
-def cost_creator(cost_info);
-
-print("Welcome to FlightCal!\nPlease state the name of your client.")
+info["name"] = input("Welcome to FlightCal!\nPlease state the name of your client.\n")
+info["age"] = input("\nHow old is your client?\n")
+temp_class = input("What class will your client {} be flying in?\nPick 1-4 in Left to Right order out of the following:\n{}\n".format(info["age"], class_list))
+info["class"] = class_list[int(temp_class)]
+print(info["class"])
