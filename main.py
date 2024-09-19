9cf1c176ef53400507b8ca0bc5910d4ff991ab6d
Antariksh Anand <antarikshanand@gmail.com>
2024-09-19 12:55:41 +1200

    Skeleton Continuation

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
+def base_price_resolver(destination):  
+    price = 0
+    if destination = "Auckland"
+        price = 300
+
+def age_resolver(age):
+    age_discount = 0
+    if age <= 18:
+        age_discount = 20
+        print("Your client is under 18, and will thus get a 20% off discount!")
+        return(age_discount)
+    elif age >= 60:
+        age_discount = 25  
+        print("Your client is over 60, and will thus get 25% off!")
+        return(age_discount)
+    return(age_discount)
+
+
 info = {
     "name" : "",
     "age" : "",
@@ -7,13 +25,17 @@ info = {
 cost_info = {
     "init_cost" : "",
     "total_seats" : "",
-    "remaining_seats" : "",
+    "remaining_seats" : ""
 }
 
-class_list = ["Economy", "Premium_Economy", "Business", "First"]
+class_list = ["Economy", "Premium Economy", "Business Class", "First Class"]
+destination_list = ["Auckland", "Christchurch", "Queenstown", "Dunedin", "Rotorua"]
 
 info["name"] = input("Welcome to FlightCal!\nPlease state the name of your client.\n")
-info["age"] = input("\nHow old is your client?\n")
-temp_class = input("What class will your client {} be flying in?\nPick 1-4 in Left to Right order out of the following:\n{}\n".format(info["age"], class_list))
-info["class"] = class_list[int(temp_class)]
-print(info["class"])
+info["age"] = int(input("\nHow old is your client?\n"))
+print("What class will your client {} be flying in?\nPick 1-4 in Left to Right order out of the following:\n".format(info["age"]))
+print(', '.join(class_list))
+temp_class = input("")
+info["class"] = class_list[int(temp_class)-1]
+
+age_resolver(info["age"])
